888c78952487865446024207f305b204afd0a23a
danurari <danurariartha09@gmail.com>
2024-01-26 01:43:27 +0800

    feat: create login action

--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -13,4 +13,28 @@ class UserControllerTest extends TestCase
        $this->get('/login')
            ->assertSeeText("Login");
    }
+
+   public function testLoginSuccess()
+   {
+       $this->post('/login', [
+           "user" => "danur",
+           "password" => "rahasia"
+       ])->assertRedirect("/")
+           ->assertSessionHas("user", "danur");
+   }
+
+    public  function testLoginValidationError()
+    {
+        $this->post('/login', [])
+            ->assertSeeText("User or password is required");
+    }
+
+    public function testLoginFailed()
+    {
+        $this->post('/login', [
+            "user" => "wrong",
+            "password" => "wrong"
+        ])->assertSeeText("User or password is wrong");
+    }
+
 }
